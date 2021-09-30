54267f6cb766e7d6295aa815290b818a0aecd118
kevinchen2002 <kaifengchen2002@gmail.com>
2021-09-30 10:25:23 -0700

    Can return from AddGameActivity
    Added button that ends the AddGameActivity

--- app/src/main/java/cmpt276/assignment2/AddGameActivity.java
+++ app/src/main/java/cmpt276/assignment2/AddGameActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddGameActivity extends AppCompatActivity {
 
@@ -12,6 +14,18 @@ public class AddGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
+
+        endActivityButton();
+    }
+
+    private void endActivityButton() {
+        Button btn = (Button) findViewById(R.id.end_add_game);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public static Intent makeIntent(Context context) {
